d5f65239abdcc1df85f0ddee83465a67356d2bfb
Malin Sofrone <malin.sofrone@gmail.com>
2019-09-03 09:42:12 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 $(function () {
     $.ajax( {
         type: 'GET',
-        url: 'https://cdn.contentful.com/spaces/lskkit434qgu?access_token=698ecc849316e21eb6bb03e4fd290619be2cb0fdac871c1964847588db0fe28a',
+        url: 'https://cdn.contentful.com/spaces/lskkit434qgu?access_token=TOKEN',
         success: function (books) {
             // console.log('success', books);
             $( ".book" ).append( "<p>Name: " + books.name + " </p>" );
